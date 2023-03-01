c6d48b0fecf1bb606865a9aa40e45e5ffbb35ff5
Emmaant <75327874+Emmaant@users.noreply.github.com>
2023-03-01 14:32:46 +0100

    Add files via upload

---
+++ arduino_to_csv.py
@@ -0,0 +1,48 @@
+
+
+import serial
+
+arduino = '/dev/cu/.usbmodem14201'
+baud = 9600
+series = serial.Serial(arduino,baud)
+
+samples = 15
+print_label = False
+filename = 'sensordata.csv'
+
+file = open(filename,'a')
+print('File created')
+
+row = 0
+sensordata = []
+while row <= samples:
+
+    line=series.readline()
+    dataString = line.decode('utf-8')
+
+    data=dataString[0:][:-1]
+    print(data)
+
+    read = data.split(",")
+    print(read)
+
+    sensordata.append(read)
+    print(sensordata)
+
+    row = row+1
+
+
+with open(fileName, 'w', encoding='UTF8', newline='') as f:
+    write = csv.writer(f)
+    write.writerows(sensordata)
+
+print("Collection of data done!")
+file.close()
+
+
+
+
+
+
+
+
