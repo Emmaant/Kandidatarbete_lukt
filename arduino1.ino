f16855e04204fae8aae29d8b2aaa43f42dcd7ef9
Emmaant <75327874+Emmaant@users.noreply.github.com>
2023-03-01 14:23:14 +0100

    Add files via upload

---
+++ arduino1.ino
@@ -0,0 +1,32 @@
+
+
+int sensor1 = A0;
+String label1 = "sensor1";
+bool label = true;
+
+
+int freq = 1000; //Collect data every 1000 ms
+
+int sensorvalue1;
+
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  pinMode(sensor1,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(label){
+    Serial.print(label1);
+    label = false;
+  }
+  sensorvalue1 = analogRead(sensor1);
+
+  //threshold?
+  Serial.print(sensorvalue1);
+  Serial.println();    // Make csv file
+
+}
